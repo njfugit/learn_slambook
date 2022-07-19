c09897d5f3bb7edf5b8b9478a36b65faf0767ac4
njfugit <jack110911@163.com>
2022-07-20 01:31:29 +0800

    update ch7

--- ch7/orb_feature_cv.cpp
+++ ch7/orb_feature_cv.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: Jack
  * @Date: 2022-07-19 00:53:10
- * @LastEditTime: 2022-07-19 22:20:49
+ * @LastEditTime: 2022-07-20 01:10:48
  * @LastEditors: your name
  * @Description: koro1FileHeader
  * @FilePath: /ch7/orb_feature_cv.cpp
@@ -14,8 +14,8 @@
 using namespace std;
 
 int main(int argc, char ** argv){
-    string img1_path = "/home/nj/DATA/slambook2/ch7/1.png";
-    string img2_path = "/home/nj/DATA/slambook2/ch7/2.png";
+    string img1_path = "/home/nj/DATA/learn_slambook/ch7/1.png";
+    string img2_path = "/home/nj/DATA/learn_slambook/ch7/2.png";
     //读取图像
     //cv::Mat img1 = cv::imread(img1_path, CV_LOAD_IMAGE_COLOR);
     //cv::Mat img2 = cv::imread(img2_path, CV_LOAD_IMAGE_COLOR);
@@ -31,7 +31,7 @@ int main(int argc, char ** argv){
     //描述子提取器
     cv::Ptr<cv::DescriptorExtractor> descriptor = cv::ORB::create();
     //描述子匹配器
-    cv::Ptr<cv::DescriptorMatcher> matcher = cv::ORB::create();
+    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
 
     //一、首先检测FAST角点位置
     chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
---
+++ ch7/orb_mine.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: Jack
+ * @Date: 2022-07-20 01:14:18
+ * @LastEditTime: 2022-07-20 01:16:48
+ * @LastEditors: your name
+ * @Description: koro1FileHeader
+ * @FilePath: /ch7/orb_mine.cpp
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
+#include<iostream>
+#include<string>
+#include<chrono>
