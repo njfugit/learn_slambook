43556f95a32cc8b480cef15c08a2abc790e2349f
njfugit <jack110911@163.com>
2022-08-07 01:10:16 +0800

    update ch8

---
+++ ch8/direct.cpp
@@ -0,0 +1,10 @@
+/*
+ * @Author: Jack
+ * @Date: 2022-08-07 01:06:53
+ * @LastEditTime: 2022-08-07 01:09:10
+ * @LastEditors: your name
+ * @Description: koro1FileHeader
+ * @FilePath: /ch8/direct.cpp
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
+#include<opencv2/opencv.hpp>
